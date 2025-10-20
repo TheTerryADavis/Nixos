45b102e540169a4c647ff2e68db2f450dc7c0d8e
CallumMcKenna <thecallummckenna@proton.me>
2025-10-19 21:33:10 -0600

    I think I'm now using open source drivers

--- modules/hardware/graphics/nvidia.nix
+++ modules/hardware/graphics/nvidia.nix
@@ -1,12 +1,18 @@
 { ... }:
 
 {
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    nvidia.modesetting.enable = true;
+  hardware = {
+    # Throwing an error, but Im too lazy to fix it right now becuase I am installing steam anyway
+    #opengl = {
+    #enable = true;
+    #  driSupport = true;
+    #  driSupport32Bit = true;
+    #};
+    nvidia = {
+      modesetting.enable = true;
+      open = true;
+    };
   };
 
-  services.xserver.videoDrivers = ["nvidia"];
+  #services.xserver.videoDrivers = ["nvidia"];
 }
